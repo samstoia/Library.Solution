4e448c49e8942ede29d9dfc35bc355cd3d14ad6c
samstoia <samstoia@gmail.com>
2019-03-06 22:07:55 -0800

    add working new and create routes, with working tests for new route controller

--- Library/Controllers/PatronsController.cs
+++ Library/Controllers/PatronsController.cs
@@ -20,5 +20,18 @@ namespace Library.Controllers
     {
       return View();
     }
+
+    [HttpPost("/patrons")]
+    public ActionResult Create(string patron_name, string address, string phone)
+    {
+        Patron newPatron = new Patron(patron_name, address, phone);
+        newPatron.Save();
+        return RedirectToAction("Index");
+    }
+    // [HttpGet("/patrons/{id}")]
+    // public ActionResult Show(int id)
+    // {
+
+    // }
   }
 }
